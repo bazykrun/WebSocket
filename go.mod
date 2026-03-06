26be62e746d2c440a5a956ca6124f0cb1b6d1513
bazykrun <bazykrun@gmail.com>
2026-03-07 02:34:41 +0600

    second

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/elliotforbes/go-websockets-tutorial
 
 go 1.25.6
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -4,14 +4,50 @@ import (
     "fmt"
     "net/http"
     "log"
+    "github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+}
+
+
 
 func homePage(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Home Page")
 }
 
+func reader(conn *websocket.Conn) {
+    for {
+	messageType, p, err := conn.ReadMessage()
+	if err != nil {
+	  log.Println(err)
+	  return
+	}
+
+	log.Println(string(p))
+
+	if err := conn.WriteMessage(messageType, p); err != nil {
+	    log.Println(err)
+	    return 
+
+
+        }
+    }
+}
+
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "WebSocket Endpoint")
+    upgrader.CheckOrigin = func(r *http.Request) bool { return true}
+
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+    }
+
+    log.Println("CLient Succesfully Connected...")
+
+    reader(ws)
 }
 
 func setupRoutes() {
