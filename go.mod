f59dd06e76721ef56fa1970d4564342a04f586cc
bagget777 <bagget777@MacBook-Pro-bagget777.local>
2026-03-07 01:29:10 +0600

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/elliotforbes/go-websockets-tutorial
+
+go 1.25.6
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "log"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "WebSocket Endpoint")
+}
+
+func setupRoutes() {
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+    fmt.Println("who is better there? yes i'm, only who win witch myself")
+    setupRoutes()
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
